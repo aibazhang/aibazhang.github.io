c526bdaec77908b52f0d0c93ec827655afa5f177
aibazhang <dtjp@tutanota.com>
2022-04-21 19:24:16 +0900

    mod: simplify md2mindmap.sh

--- md2mindmap.sh
+++ md2mindmap.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
-
-files=`find ./ -type f -name "*.md" ! -name "README.md" ! -path "./node_modules/*"`
 mkdir -p ./html/tech ./html/nontech 
+files=`find ./ -type f -name "*.md" ! -name "README.md" ! -path "./node_modules/*"`
 
 for file in $files;
 do
-  # tr
   output_file="${file/md/html}"
-  echo "transforming $file to $output_file" && node_modules/.bin/markmap $file -o $output_file.html --no-open
+  echo "transforming $file to $output_file"
+  node_modules/.bin/markmap $file -o $output_file.html --no-open #tr
 done
 
-list=`find ./html -type f -name "*.html" ! -name "index.html" | awk '{print "\""$0"\","}'`
-
-# sed "s|HTML_FILE_LIST|$list|g" index.html.sample
-awk -v list="$list" '{sub(/HTML_FILE_LIST/, list); print}' index.html.sample > index.html
-
-# https://github.com/jpadfield/simple-site/blob/master/.github/workflows/build.yml
+find ./html -type f -name "*.html" ! -name "index.html" | \
+  awk '{print "\""$0"\","}' | \
+  xargs -0 -I{} awk -v list={} '{sub(/HTML_FILE_LIST/, list); print}' \
+  index.html.sample > index.html
